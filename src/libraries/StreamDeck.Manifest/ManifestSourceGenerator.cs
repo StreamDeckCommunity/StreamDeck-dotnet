f2fbe0b0994ba9ed45566bb563d62430664dee80
Richard Herman <geekyeggo@users.noreply.github.com>
2022-02-22 16:37:17 +0000

    Refactor source generator

--- src/libraries/StreamDeck.Manifest/ManifestSourceGenerator.cs
+++ src/libraries/StreamDeck.Manifest/ManifestSourceGenerator.cs
@@ -5,7 +5,9 @@
     using System.Linq;
     using System.Text;
     using Microsoft.CodeAnalysis;
+    using StreamDeck.Manifest.Extensions;
     using StreamDeck.Manifest.Serialization;
+    using System.Collections.Generic;
 
     /// <summary>
     /// Provides auto-generation of the manifest.json file that accompanies a Stream Deck plugin.
@@ -16,41 +18,47 @@
         /// <inheritdoc/>
         public void Execute(GeneratorExecutionContext context)
         {
-            // Ensure we have a registered context receiver; this is responsible for discovering actions and states.
-            if (context.SyntaxContextReceiver is ManifestBuilder builder)
+            // Should only generate the manifest file if we have a receiver.
+            if (!(context.SyntaxContextReceiver is PluginActionSyntaxReceiver syntaxReceiver)
+                || context.CancellationToken.IsCancellationRequested)
             {
-                try
-                {
-                    // Ensure we know where the manifest.json file is located.
-                    var canWrite = this.TryGetFilePath(context, out var manifestFilePath);
-                    if (!canWrite)
-                    {
-                        context.ReportDiagnostic(Diagnostics.MissingManifestFile);
-                    }
+                return;
+            }
 
-                    // Attempt to parse the manifest information from the context.
-                    if (builder.TryBuild(context, out var manifest))
-                    {
-                        if (canWrite)
-                        {
-                            File.WriteAllText(manifestFilePath, JsonSerializer.Serialize(manifest), Encoding.UTF8);
-                        }
-                    }
-                    else
-                    {
-                        context.ReportDiagnostic(Diagnostics.MissingManifestAttribute);
-                    }
-                }
-                catch (Exception ex)
+            try
+            {
+                // Ensure we know where the manifest.json file is located.
+                if (!this.TryGetFilePath(context, out var filePath))
                 {
-                    context.ReportDiagnostic(Diagnostics.Exception(ex));
+                    context.ReportMissingManifestFile();
+                    return;
                 }
+
+                // Ensure we have the manifest attribute associated with the assembly.
+                if (!context.Compilation.Assembly.TryGetAttribute<PluginManifestAttribute>(out var manifestAttr))
+                {
+                    context.ReportMissingManifestAttribute();
+                    return;
+                }
+
+                // Construct the manifest, and the actions associated with it
+                var manifest = new PluginManifestAttribute(context.Compilation.Assembly);
+                manifestAttr.Populate(manifest);
+                manifest.Actions.AddRange(this.GetActions(context, syntaxReceiver));
+
+                // Write the manifest file.
+                var json = JsonSerializer.Serialize(manifest);
+                File.WriteAllText(filePath, json, Encoding.UTF8);
+            }
+            catch (Exception ex)
+            {
+                context.ReportException(ex);
             }
         }
 
         /// <inheritdoc/>
         public void Initialize(GeneratorInitializationContext context)
-            => context.RegisterForSyntaxNotifications(() => new ManifestBuilder());
+            => context.RegisterForSyntaxNotifications(() => new PluginActionSyntaxReceiver());
 
         /// <summary>
         /// Attempts to get the manifest.json file path from the additional files defined in the <paramref name="context"/>.
@@ -62,6 +70,47 @@
         {
             filePath = context.AdditionalFiles.FirstOrDefault(a => a.Path.EndsWith("manifest.json", StringComparison.OrdinalIgnoreCase))?.Path;
             return filePath != null;
+        }
+
+        /// <summary>
+        /// Gets the actions associated with the generator.
+        /// </summary>
+        /// <param name="context">The generator execution context.</param>
+        /// <param name="syntaxReceiver">The syntax receiver that contains the actions.</param>
+        /// <returns>The actions discovered whilst traversing the compilation nodes.</returns>
+        private IEnumerable<PluginActionAttribute> GetActions(GeneratorExecutionContext context, PluginActionSyntaxReceiver syntaxReceiver)
+        {
+            foreach (var actionDeclaration in syntaxReceiver.Actions)
+            {
+                var action = actionDeclaration.AttributeData.CreateInstance<PluginActionAttribute>();
+                var states = actionDeclaration.Symbol.GetAttributes<PluginActionStateAttribute>().ToArray();
+
+                if (states.Length > 0)
+                {
+                    // When there is a state image defined, and custom states, warn of duplication.
+                    if (action.States.Count > 0)
+                    {
+                        context.ReportStateImageValueObsolete(actionDeclaration.Symbol.Locations.First());
+                    }
+
+                    action.States.Clear();
+                    action.States.AddRange(states.Select(s => s.CreateInstance<PluginActionStateAttribute>()).Take(2));
+                }
+
+                // Ensure we have at least 1 action state.
+                if (action.States.Count == 0)
+                {
+                    context.ReportNoActionStatesDefined(actionDeclaration.Symbol.Locations.First());
+                }
+
+                // Ensure we dont have more than 2 action states.
+                if (action.States.Count > 2)
+                {
+                    context.ReportTooManyActionStates(actionDeclaration.Symbol.Locations.First());
+                }
+
+                yield return action;
+            }
         }
     }
 }
---
+++ src/libraries/StreamDeck.Manifest/PluginActionSyntaxReceiver.cs
@@ -0,0 +1,51 @@
+﻿namespace StreamDeck.Manifest
+{
+    using System.Collections.ObjectModel;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using StreamDeck.Manifest.Extensions;
+
+    /// <summary>
+    /// Provides a <see cref="ISyntaxContextReceiver"/> that is capable of discovering classes that represent Stream Deck plugin actions.
+    /// </summary>
+    internal class PluginActionSyntaxReceiver : ISyntaxContextReceiver
+    {
+        /// <summary>
+        /// Gets the classes that represent Stream Deck plugin actions.
+        /// </summary>
+        internal Collection<DecoratedSymbolDeclaration> Actions { get; } = new Collection<DecoratedSymbolDeclaration>();
+
+        /// <inheritdoc/>
+        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+        {
+            if (context.Node is ClassDeclarationSyntax _)
+            {
+                var symbol = context.SemanticModel.GetDeclaredSymbol(context.Node);
+                if (symbol.TryGetAttribute<PluginActionAttribute>(out var attrData))
+                {
+                    this.Actions.Add(new DecoratedSymbolDeclaration
+                    {
+                        Symbol = symbol,
+                        AttributeData = attrData
+                    });
+                }
+            }
+        }
+
+        /// <summary>
+        /// Provides information about a symbol that was discovered as the result of its attribute data.
+        /// </summary>
+        internal struct DecoratedSymbolDeclaration
+        {
+            /// <summary>
+            /// Gets or sets the symbol.
+            /// </summary>
+            public ISymbol Symbol { get; set; }
+
+            /// <summary>
+            /// Gets or sets the attribute data.
+            /// </summary>
+            public AttributeData AttributeData { get; set; }
+        }
+    }
+}
